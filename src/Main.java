e0b2b13780556faa755a95706be7f43986517ac3
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-10-24 15:15:04 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Kieb", 41, "ưe");
+        Student student1 = new Student("ưefwef", 24, "ffdrgefwfeb");
+        Student student2 = new Student("fsfe", 2, "efwefwef");
+        Student student3 = new Student("adfsdf", 13, "fvbf");
+
+        List<Student> lists = new ArrayList<>();
+        lists.add(student);
+        lists.add(student1);
+        lists.add(student2);
+        lists.add(student3);
+        Collections.sort(lists);
+        for (Student st : lists) {
+            System.out.println(st.toString());
+        }
+        AgeComparator ageComparator = new AgeComparator();
+        Collections.sort(lists, ageComparator);
+        System.out.println("So sanh theo tuoi:");
+        for (Student st : lists) {
+            System.out.println(st.toString());
+        }
+    }
+}
